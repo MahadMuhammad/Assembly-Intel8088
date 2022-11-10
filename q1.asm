0c6f9ddf77e9b748f2ef6991e0df10326e1fcc24
Muhammad Mahad <mahadtxt@gmail.com>
2022-11-11 00:39:55 +0500

    Some updates

--- q1.asm
+++ q1.asm
@@ -1,71 +1,43 @@
-; hello world at desired screen location 
-[org 0x0100] 
-    jmp start 
-message: db 'hello world' ; string to be printed 
-length: dw 11 ; length of the string 
-; subroutine to clear the screen 
-clrscr: 
-    push es 
-    push ax 
-    push di 
-    mov ax, 0xb800 
-    mov es, ax ; point es to video base 
-    mov di, 0 ; point di to top left column 
-nextloc: 
-    mov word [es:di], 0x0720 ; clear next char on screen 
-    add di, 2 ; move to next screen location 
-    cmp di, 4000 ; has the whole screen cleared 
-    jne nextloc ; if no clear next position 
-    pop di 
-    pop ax 
-    pop es 
-    ret
-; subroutine to print a string at top left of screen 
-; takes x position, y position, string attribute, address of string 
-; and its length as parameters 
-printstr: 
-    push bp 
-    mov bp, sp 
-    push es 
-    push ax 
-    push cx 
-    push si 
-    push di 
+[org 0x0100]
 
-    mov ax, 0xb800 
-    mov es, ax ; point es to video base 
-    mov al, 80 ; load al with columns per row 
-    mul byte [bp+10] ; multiply with y position 
-    add ax, [bp+12] ; add x position 
-    shl ax, 1 ; turn into byte offset 
-    mov di,ax ; point di to required location 
-    mov si, [bp+6] ; point si to string 
-    mov cx, [bp+4] ; load length of string in cx 
-    mov ah, [bp+8] ; load attribute in ah 
-nextchar: 
-    mov al, [si] ; load next char of string 
-    mov [es:di], ax ; show this char on screen 
-    add di, 2 ; move to next screen location
-    add si, 1 ; move to next char in string 
-    loop nextchar ; repeat the operation cx times 
-    pop di 
-    pop si 
-    pop cx 
-    pop ax 
-    pop es 
-    pop bp 
-    ret 10 
+jmp  start
+
+clrscr:
+    push es
+    push ax
+    push cx
+    push di
+
+    mov  ax, 0xb800
+    mov  es, ax
+    xor  di, di
+    mov  ah, 0x07
+    mov al,'M'
+    mov  cx, 2000
+
+                    ; auto-increment mode 
+    rep stosw           ; rep cx times, store words 
+                        ; source is ax for word, al for bytes 
+                        ; destination is es:di 
+                        ; inc/dec di as well by 2 bytes 
+
+    pop di
+    pop  cx
+    pop  ax
+    pop  es
+    ret
+
+
 start: 
-    call clrscr ; call the clrscr subroutine 
-    mov ax, 40
-    push ax ; push x position 
-    mov ax, 12
-    push ax ; push y position 
-    mov ax, 1 ; blue on black attribute 
-    push ax ; push attribute 
-    mov ax, message 
-    push ax ; push address of message 
-    push word [length] ; push message length 
-    call printstr ; call the printstr subroutine 
- mov ax, 0x4c00 ; terminate program 
- int 0x21 
+
+    mov  ax, 0xb800
+    mov  es, ax
+    xor  di, di
+    mov  ah, 0x07
+    mov al,0x20
+    mov  cx, 2000
+
+    cld           ; auto-increment mode 
+    rep stosw           ; rep cx times, store words 
+    mov  ax, 0x4c00
+    int  0x21
