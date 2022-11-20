7dcd798d8e8365ed5811c1ee7995d57fa81edc72
Muhammad Mahad <mahadtxt@gmail.com>
2022-11-20 16:34:19 +0500

    Implemented Intro Function
    Added Intro function in my TIC TAC TOE assembly which First Prints DOT on screen, wait for user & prints grey color on screen.

--- Project/p.asm
+++ Project/p.asm
@@ -30,7 +30,7 @@
 ;   2. If a player chooses a position that is not on the board
 ;      the game will display an error message and the player
 ;      will have to choose another position
-; WINNING / DRAW:
+; WINNING / DRAW :
 ;   1. If a player gets 4 X's or O's in a row, column, or diagonal
 ;      the game will display a message and the player will win
 ;   2. If the board is full and no player has won, the game will
@@ -87,13 +87,112 @@
     jmp main        ; jump to main
 
 ;   --------------------------------------------------------
-;   INCLUDES
+;   DEFINITIONS
+;   --------------------------------------------------------
+
+board:  
+        db 0,0,0,0          ; 1 | 2 | 3 | 4
+        db 0,0,0,0          ; Q | W | E | R
+        db 0,0,0,0          ; A | S | D | F
+        db 0,0,0,0          ; Z | X | C | V 
+
+TURN:  db 1                 ;   1 = Player 1, 2 = Player 2, 3 = Game Over 
+
+P1WIN: db 'Player-1 Wins!'  ;   Player 1 wins message
+
+P2WIN: db 'Player-2 Wins!'  ;   Player 2 wins message
+
+DRAW:  db 'Draw!'           ;   Game Draw message
+
+ERROR: db 'Error!'          ;   Game Error message
+
+;   --------------------------------------------------------
+;   BASIC FUNCTIONS
+;   --------------------------------------------------------
+
+Intro:                             ;   First Prints DOT on screen, wait for user & prints grey color on screen 
+                                   ;   Used in main function
+            call Dot               ;   Call the Dot function
+            call AnyKeyPress       ;   Call the AnyKeyPress function
+            pusha                  ;   Save the registers
+
+            mov ax,0xb800   ;   Set the video memory address
+            mov es,ax       ;   Set the video memory segment
+            mov di,0        ;   Set a pointer to the start of the video memory
+            mov ah,0x77     ;   set color ASCII code (0111 0111)
+            mov al,20h      ;   set the blank character     
+            mov cx,2000     ;   set the number of characters to write
+            cld             ;   clear direction flag
+            rep stosw       ;   Fill the video memory with the attribute byte
+
+    
+            popa            ;   Restore the registers
+            ret
 ;   --------------------------------------------------------
 
 
 
-   
+
+
+;___________________________________________________________
+;   --------------------------------------------------------
+;   EXTRA FUNCTIONS
+;   --------------------------------------------------------
+Dot:                        ;   Prints dots on screen  (Used in Intro function)
+        pusha               ;   Save the registers  
+
+        mov ax,0xb800       ;   Set the video memory address
+        mov es,ax           ;   Set the video memory segment
+        mov di,0            ;   Set a pointer to the start of the video memory
+        mov ah,0x0e         ;   Set the attribute byte
+        mov al,0x07         ;   Set the color to white
+        mov [es:di],al      ;   Set the color to white
+        mov cx,2000
+
+        cld
+        rep stosw           ;   Fill the video memory with the attribute byte
+
+        popa                ;   Restore the registers
+        ret
+;   --------------------------------------------------------
+AnyKeyPress:                ;   Wait for any key press  (Used in Intro function)
+        pusha               ;   Save the registers
+
+        mov ah,0x00         ;   Set the interrupt number
+        int 0x16            ;   Call the interrupt
+
+        popa                ;   Restore the registers
+        ret
+;   --------------------------------------------------------
+Delay:                    ;   Delay function 
+        pusha             ;   Save the registers
+	    mov cx,0xffff     ;   Set the counter to 0xffff
+
+	    LoopDelay:                  ;   LoopDelay label
+	    	    loop LoopDelay      ;   Loop the LoopDelay label
+        
+        popa               ;   Restore the registers
+        ret                ;   Return to the caller
+;   --------------------------------------------------------
+
+;___________________________________________________________
+;   --------------------------------------------------------
+;   FUNCTION: main
+;   --------------------------------------------------------
 main:
+    call Intro           ; First Prints DOT on screen, wait for user & prints grey color on screen
+    ;call initBoard      ;   Initialize the board
+    ;call displayBoard   ;   Display the board
+    ;call player1        ;   Player 1 turn
+    ;call player2        ;   Player 2 turn
+    ;call checkWin       ;   Check if the game is won
+    ;call checkDraw      ;   Check if the game is a draw
+    ;call displayBoard   ;   Display the board
+    ;call displayResult  ;   Display the result
+    ;ret                 ;   Return to the caller
+
+
+
 
 
 
